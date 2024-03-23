afdf9a5c6a64df00f46cc284ec2fc396b5225a5b
Thomas Johnson <NTmatter@gmail.com>
2024-03-23 16:41:57 +0100

    Code initially submitted to issue report.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // let res = reqwest::get("https://www.figma.com/").await?;
+    // let res = reqwest::get("https://api.figma.com/v1/activity_logs").await?;
+    let res = reqwest::get("https://api.figma.com/").await?;
+    let status = res.status();
+    let res = res.text().await?;
+
+    if status == 200 {
+        println!("Success!");
+    } else {
+        eprintln!("Failed to retrieve data: {status}");
+        dbg!(res);
+    }
+
+    Ok(())
 }
