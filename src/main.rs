21e74243c558d382181ba61b4e233470d9768eaf
Thomas Johnson <NTmatter@gmail.com>
2024-03-23 16:51:34 +0100

    Use both reqwest 0.11 and 0.12

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,26 @@
 async fn main() -> anyhow::Result<()> {
     // let res = reqwest::get("https://www.figma.com/").await?;
     // let res = reqwest::get("https://api.figma.com/v1/activity_logs").await?;
-    let res = reqwest::get("https://api.figma.com/").await?;
+    let res = reqwest_11::get("https://api.figma.com/").await?;
     let status = res.status();
-    let res = res.text().await?;
 
     if status == 200 {
         println!("Success!");
     } else {
         eprintln!("Failed to retrieve data: {status}");
-        dbg!(res);
     }
+    dbg!(res);
+
+    let res = reqwest_12::get("https://api.figma.com/").await?;
+    let status = res.status();
+
+    if status == 200 {
+        println!("Success!");
+    } else {
+        eprintln!("Failed to retrieve data: {status}");
+    }
+    dbg!(res);
 
     Ok(())
 }
+
