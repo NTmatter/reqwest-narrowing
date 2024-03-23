159b8e6146ea150a1177c6c1d22daa78ec60ddbc
Thomas Johnson <NTmatter@gmail.com>
2024-03-23 17:27:18 +0100

    Simple tests for various endpoints and Reqwest 0.11/0.12

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // let res = reqwest::get("https://www.figma.com/").await?;
-    // let res = reqwest::get("https://api.figma.com/v1/activity_logs").await?;
     let res = reqwest_11::get("https://api.figma.com/").await?;
     let status = res.status();
 
@@ -10,18 +8,43 @@ async fn main() -> anyhow::Result<()> {
     } else {
         eprintln!("Failed to retrieve data: {status}");
     }
-    dbg!(res);
-
-    let res = reqwest_12::get("https://api.figma.com/").await?;
-    let status = res.status();
-
-    if status == 200 {
-        println!("Success!");
-    } else {
-        eprintln!("Failed to retrieve data: {status}");
-    }
-    dbg!(res);
 
     Ok(())
+}
+
+#[tokio::test]
+async fn figma_www_root_11() {
+    let res = reqwest_11::get("https://www.figma.com/").await.unwrap() ;
+    assert_eq!(res.status(), 200, "Response failed: {res:#?}");
+}
+
+#[tokio::test]
+async fn figma_www_root_12() {
+    let res = reqwest_12::get("https://www.figma.com/").await.unwrap() ;
+    assert_eq!(res.status(), 200, "Response failed: {res:#?}");
+}
+
+#[tokio::test]
+async fn figma_api_root_11() {
+    let res = reqwest_11::get("https://api.figma.com/").await.unwrap() ;
+    assert_eq!(res.status(), 200, "Response failed: {res:#?}");
+}
+
+#[tokio::test]
+async fn figma_api_root_12() {
+    let res = reqwest_12::get("https://api.figma.com/").await.unwrap() ;
+    assert_eq!(res.status(), 200, "Response failed: {res:#?}");
+}
+
+#[tokio::test]
+async fn figma_api_me_11() {
+    let res = reqwest_11::get("https://api.figma.com/v1/me").await.unwrap() ;
+    assert_eq!(res.status(), 403, "Expected 403 Forbidden: {res:#?}");
+}
+
+#[tokio::test]
+async fn figma_api_api_me_12() {
+    let res = reqwest_12::get("https://api.figma.com/v1/me").await.unwrap() ;
+    assert_eq!(res.status(), 403, "Expected 403 Forbidden: {res:#?}");
 }
 
